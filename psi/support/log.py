19d9425625b7f5cafe92ef06059f6890c990dda7
Iain Rosen <hello@iainrosen.me>
2022-02-24 14:51:36 -0800

    added init to log file

--- psi/support/log.py
+++ psi/support/log.py
@@ -4,7 +4,12 @@ class logFile:
         self.logPath = logPath
         self.logType = logType
         self.logFormat = logFormat
+        #init the log file
+        with open(logPath, 'a') as logFileWriterInit:
+            logFileWriterInit.write('Start of log at '+str(datetime.now())+'. ['+logType+' '+logFormat+']')
+            logFileWriterInit.write('\n')
     def log(self, logLevel, logMsg):
         logTime = str(datetime.now())
         with open(self.logPath, 'a') as logFileWriter:
             logFileWriter.write('['+str(logTime)+']'+'['+logLevel+']'+' '+logMsg)
+            logFileWriter.write('\n')
